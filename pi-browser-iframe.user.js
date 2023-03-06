59cc5f5527fcf88a7cba2e52e99c039067b8a573
Brewal Derouet <brewal_derouet@hotmail.fr>
2023-03-06 20:49:20 +0100

    feat: set Pi Browser User Agent in iframe

--- pi-browser-iframe.user.js
+++ pi-browser-iframe.user.js
@@ -18,6 +18,24 @@
 	}
 
 	if (window.parent !== window.top) {
+		const userAgentProperty = {
+			get: () =>
+				'Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.153 Mobile Safari/537.36 PiBrowser/1.6.1',
+		};
+		const vendorProperty = {
+			get: () => 'Google Inc.',
+		};
+
+		try {
+			Object.defineProperty(window.navigator, 'userAgent', userAgentProperty);
+			Object.defineProperty(window.navigator, 'vendor', vendorProperty);
+		} catch {
+			window.navigator = Object.create(navigator, {
+				userAgent: userAgentProperty,
+				vendor: vendorProperty,
+			});
+		}
+
 		return;
 	}
 

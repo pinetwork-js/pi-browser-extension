c03d4026e977aff142667a56606a735a051897fa
b-derouet <brewal_derouet@hotmail.fr>
2023-03-22 00:47:59 +0000

    Deploying to gh-pages from @ PiNetwork-js/pi-browser-extension@6e8e8af3e851323dde30b901d807fd295fb839d8 🚀

--- core/pi-browser-extension-core.meta.js
+++ core/pi-browser-extension-core.meta.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name        pi-browser-extension-core
 // @description Script to configurate Pi Apps inside Pi Browser iframe
-// @version     1.1.1
+// @version     1.1.2
 // @namespace   https://github.com/pinetwork-js/pi-browser-extension
+// @homepage    https://pinetwork-js.github.io/pi-browser-extension/core/
 // @author      B-Derouet <brewal_derouet@hotmail.fr>
 // @license     MIT
 // @icon64      https://www.google.com/s2/favicons?sz=64&domain=minepi.com
 // @match       https://app-cdn.minepi.com/*
 // @noframes    
 // @unwrap      
+// @updateURL   https://pinetwork-js.github.io/pi-browser-extension/core/pi-browser-extension-core.meta.js
+// @downloadURL https://pinetwork-js.github.io/pi-browser-extension/core/pi-browser-extension-core.user.js
 // ==/UserScript==

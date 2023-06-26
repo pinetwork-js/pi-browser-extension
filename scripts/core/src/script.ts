d94ed22db0a10b9efcec7704fd5abc5d3e4ec5ee
b-derouet <brewal_derouet@hotmail.fr>
2023-06-26 23:00:50 +0200

    feat: update config when updated or during login

--- scripts/core/src/script.ts
+++ scripts/core/src/script.ts
@@ -1,13 +1,29 @@
 import { setupBrowser } from './setup-browser';
-import { setupConfig } from './setup-config';
+import { browserConfig, loginConfig, setupConfig } from './setup-config';
 
 export function script() {
-	if (!localStorage.getItem('@pi:webview_ui_behaviors')) {
-		setupConfig();
+	const userConfig = localStorage.getItem('@pi:webview_ui_behaviors');
+	const token = localStorage.getItem('mobile-app-webview-ui_access-token');
+	const loggedIn = localStorage.getItem('@pi:logged-in');
+
+	if (!!token && userConfig !== JSON.stringify(browserConfig)) {
+		setupConfig(browserConfig);
+	} else if (!token && !loggedIn) {
+		setupConfig(loginConfig);
+
+		localStorage.setItem('@pi:logged-in', 'false');
+
+		window.location.href = 'https://app-cdn.minepi.com/mobile-app-ui/node-signin';
 	}
 
-	if (window.location.href === 'https://app-cdn.minepi.com/mobile-app-ui/close.html') {
+	if (window.location.href !== 'https://app-cdn.minepi.com/mobile-app-ui/node-signin' && loggedIn === 'false') {
 		window.location.href = 'https://app-cdn.minepi.com/mobile-app-ui/node-signin';
+	}
+
+	if (token && loggedIn === 'false') {
+		localStorage.setItem('@pi:logged-in', 'true');
+
+		window.location.href = 'https://app-cdn.minepi.com/browser';
 	}
 
 	if (!window.location.href.startsWith('https://app-cdn.minepi.com/browser')) {
--- scripts/core/src/setup-config.ts
+++ scripts/core/src/setup-config.ts
@@ -1,23 +1,29 @@
-export function setupConfig() {
-	const config = {
-		platform: {
-			desktop: true,
-			os: 'win32',
-			version: '10.0.0',
-			arch: 'x64',
-			uuid: '',
-			hostApp: 'pi-node',
-		},
-		bundleData: {
-			versionNumber: '1.6.1',
-			buildNumber: '21',
-		},
-		nativeSupport: {
-			requestPermissions: true,
-			inlineMediaPlayback: true,
-		},
-	};
+export const loginConfig = {
+	platform: {
+		desktop: true,
+		os: 'win32',
+		version: '10.0.0',
+		arch: 'x64',
+		uuid: '',
+		hostApp: 'pi-node',
+	},
+	bundleData: {
+		versionNumber: '1.6.1',
+		buildNumber: '21',
+	},
+	nativeSupport: {
+		requestPermissions: true,
+		inlineMediaPlayback: true,
+	},
+};
 
+const temporaryConfig = structuredClone(loginConfig);
+temporaryConfig.platform.hostApp = 'pi-browser';
+
+export const browserConfig = temporaryConfig;
+
+export function setupConfig(config: typeof loginConfig) {
 	localStorage.setItem('@pi:webview_ui_behaviors', JSON.stringify(config));
+
 	window.location.reload();
 }

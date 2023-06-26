c56938828a9b5413f4e889844aeaa8db9473d4b7
b-derouet <brewal_derouet@hotmail.fr>
2023-06-26 21:55:40 +0200

    fix: redirect to welcome page when url is empty

--- scripts/core/src/get-url-from-link.ts
+++ scripts/core/src/get-url-from-link.ts
@@ -4,5 +4,9 @@ export function getUrlFromLink() {
 	const url = new URL(window.location.href);
 	const urlParameter = url.searchParams.get('url');
 
-	return (urlParameter ?? pages.welcome).replace('pi://', 'https://');
+	if (!urlParameter) {
+		return pages.welcome.replace('pi://', 'https://');
+	}
+
+	return urlParameter.replace('pi://', 'https://');
 }
